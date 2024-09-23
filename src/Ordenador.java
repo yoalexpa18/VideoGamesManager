67519bb09c3aba3587d3c6baf8365a24ae76f19c
yoalexpa18 <yo.aw.alexpa18@gmail.com>
2024-09-23 17:00:35 +0200

    Inicializacion de agragar juegos a un txt

--- src/Ordenador.java
+++ src/Ordenador.java
@@ -1,25 +1,35 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Ordenador implements Console {
 
     Plataformas plataforma;
 
     @Override
-    public void switchOn(){
+    public void switchOn() {
         System.out.printf("%s encendido%n", plataforma.Ordenador);
     }
+
     @Override
-    public void switchOff(){
+    public void switchOff() {
         System.out.printf("%s apagado%n", plataforma.Ordenador);
     }
+
     @Override
-    public void installGame(Videojuegos videojuegos){
-        System.out.printf("%s installado en %s.%n",videojuegos, plataforma.Ordenador);
+    public void installGame(Videojuegos videojuegos) {
+        System.out.printf("%s installado en %s.%n", videojuegos, plataforma.Ordenador);
     }
+
     @Override
-    public void uninstallGame(Videojuegos videojuegos){
-        System.out.printf("%s desinstalado en %s %n",videojuegos.nombre, plataforma.Ordenador);
+    public void uninstallGame(Videojuegos videojuegos) {
+        ValidarConsola(videojuegos.plataforma);
+        System.out.printf("%s desinstalado en %s %n", videojuegos.nombre, plataforma.Ordenador);
     }
+
     @Override
-    public void playGame(){
+    public void playGame() {
         System.out.printf("%s inicia el juego%n", plataforma.Ordenador);
     }
 
@@ -27,4 +37,25 @@ public class Ordenador implements Console {
     public String getPlataforma() {
         return "" + plataforma.Ordenador;
     }
+
+    private void ValidarConsola(Plataformas plataforma) {
+
+        try {
+            File instalar = new File("Ordenador.txt");
+            FileWriter fw = new FileWriter(instalar);
+
+            if (plataforma == Plataformas.Ordenador) {
+                System.out.printf("Juego compatible para %s%n", plataforma.Ordenador);
+                BufferedWriter ecribir = new BufferedWriter(fw);
+
+
+            } else {
+                System.out.printf("Juego incompatible para %s%n", plataforma.Ordenador);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
